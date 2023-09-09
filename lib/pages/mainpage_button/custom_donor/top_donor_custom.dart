bb28b762938ea46f19187c892966f31eb13a78d6
Nurujjaman329 <mdnurujjaman329@gmail.com>
2023-09-09 23:46:51 +0600

    create page

---
+++ lib/pages/mainpage_button/custom_donor/top_donor_custom.dart
@@ -0,0 +1,103 @@
+import 'package:firstapp/utils/text_style.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TopDonorCustom {
+  Widget CardList(index, BuildContext context) {
+    // var _height = MediaQuery.of(context).size.height;
+    var _width = MediaQuery.of(context).size.width;
+    return Card(
+      elevation: 7,
+      shadowColor: Colors.black,
+      color: Colors.white,
+      child: Padding(
+        padding: EdgeInsets.only(left: 3, bottom: 8, top: 8),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                SizedBox(
+                  width: _width * .20,
+                  child: CircleAvatar(
+                    radius: 30,
+                  ),
+                ),
+                Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: _width * .27,
+                          child: Text("Top Donor Name"),
+                        ),
+                        Text(": "),
+                        SizedBox(
+                          width: _width * .31,
+                          child: Text("Md Nurujjaman"),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: _width * .27,
+                          child: Text("Phone Number"),
+                        ),
+                        Text(": "),
+                        SizedBox(
+                          width: _width * .31,
+                          child: Text("01957073942"),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: _width * .27,
+                          child: Text("Gender"),
+                        ),
+                        Text(": "),
+                        SizedBox(
+                          width: _width * .31,
+                          child: Text("male"),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: _width * .16,
+                  child: Column(
+                    children: [
+                      Text(
+                        "A+",
+                        style: Styles.small,
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Icon(
+                        Icons.call,
+                        size: 30,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/mainpage_button/top_donors.dart
+++ lib/pages/mainpage_button/top_donors.dart
@@ -1,5 +1,4 @@
-import 'package:firstapp/utils/color_static.dart';
-import 'package:firstapp/utils/text_style.dart';
+import 'package:firstapp/pages/mainpage_button/custom_donor/top_donor_custom.dart';
 import 'package:firstapp/utils/textstyle_static.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -20,7 +19,7 @@ class _TopDonorState extends State<TopDonor> {
         elevation: 0,
         backgroundColor: Colors.white,
         title: Text(
-          "Patient Request",
+          "Top Donor",
           style: TextStyles.app_bar,
         ),
         actions: [Icon(Icons.ring_volume_rounded)],
@@ -28,48 +27,7 @@ class _TopDonorState extends State<TopDonor> {
       body: ListView.builder(
           itemCount: 5,
           itemBuilder: (BuildContext context, int index) {
-            return Padding(
-              padding: EdgeInsets.all(4.0),
-              child: Card(
-                elevation: 50,
-                shadowColor: Colors.black,
-                color: Colors.white,
-                child: SizedBox(
-                  height: 100,
-                  width: double.infinity,
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Icon(Icons.man_2_rounded),
-                    ),
-                    title: Padding(
-                      padding: EdgeInsets.only(top: 8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "{allDat[index\n]['name].tostr\ning()} :",
-                            style: Styles.maxaverage,
-                          ),
-                          SizedBox(
-                            height: 15,
-                          ),
-                        ],
-                      ),
-                    ),
-                    trailing: Column(
-                      children: [
-                        Icon(Icons.star, color: ColorCode.appbar_color2),
-                        Text(
-                          "{allDat[index]['name].tostring()} :",
-                          style: Styles.extraextrasmall,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            );
+            return TopDonorCustom().CardList(index, context);
           }),
     );
   }
