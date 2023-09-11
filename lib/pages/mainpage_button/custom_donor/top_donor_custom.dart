77045c64774517931b39357c72ec4b53eba6d4e0
Nurujjaman329 <mdnurujjaman329@gmail.com>
2023-09-11 08:53:12 +0600

    top donor page

--- lib/pages/mainpage_button/custom_donor/top_donor_custom.dart
+++ lib/pages/mainpage_button/custom_donor/top_donor_custom.dart
@@ -6,114 +6,120 @@ class TopDonorCustom {
   Widget CardList(index, BuildContext context) {
     // var _height = MediaQuery.of(context).size.height;
     var _width = MediaQuery.of(context).size.width;
-    return Card(
-      elevation: 7,
-      shadowColor: Colors.black,
-      color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.only(left: 3, bottom: 8, top: 8),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                SizedBox(
-                  width: _width * .20,
-                  child: const CircleAvatar(
-                    backgroundColor: ColorCode.primary_color,
-                    radius: 30,
+    return Padding(
+      padding: const EdgeInsets.only(left: 2, bottom: 3, top: 3),
+      child: Card(
+        elevation: 2,
+        shadowColor: Colors.black,
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 3, bottom: 8, top: 8),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  SizedBox(
+                    width: _width * .20,
+                    child: const CircleAvatar(
+                      backgroundColor: ColorCode.primary_color,
+                      radius: 30,
+                    ),
                   ),
-                ),
-                Column(
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          width: _width * .30,
-                          child: Text(
-                            "Top Donor Name",
-                            style: Style.extrasmallerD14,
-                          ),
-                        ),
-                        Text(
-                          ": ",
-                          style: Style.extrasmallerD14,
-                        ),
-                        SizedBox(
-                          width: _width * .29,
-                          child: const Text("Md Nurujjaman"),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: _width * .30,
-                          child: Text(
-                            "Phone Number",
-                            style: Style.extrasmallerD14,
-                          ),
-                        ),
-                        Text(
-                          ": ",
-                          style: Style.extrasmallerD14,
-                        ),
-                        SizedBox(
-                          width: _width * .29,
-                          child: const Text("01957073942"),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: _width * .30,
-                          child: Text(
-                            "Gender",
-                            style: Style.extrasmallerD14,
-                          ),
-                        ),
-                        Text(": "),
-                        SizedBox(
-                          width: _width * .29,
-                          child: Text(
-                            "male",
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  width: _width * .15,
-                  child: Column(
+                  Column(
                     children: [
-                      Text(
-                        "A+",
-                        style: Style.average,
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: _width * .30,
+                            child: Text(
+                              "Top Donor Name",
+                              style: Style.extrasmallerD14,
+                            ),
+                          ),
+                          Text(
+                            ": ",
+                            style: Style.extrasmallerD14,
+                          ),
+                          SizedBox(
+                            width: _width * .29,
+                            child: const Text("Md Nurujjaman"),
+                          ),
+                        ],
                       ),
                       const SizedBox(
-                        height: 15,
+                        height: 5,
                       ),
-                      const Icon(
-                        Icons.call,
-                        size: 30,
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: _width * .30,
+                            child: Text(
+                              "Phone Number",
+                              style: Style.extrasmallerD14,
+                            ),
+                          ),
+                          Text(
+                            ": ",
+                            style: Style.extrasmallerD14,
+                          ),
+                          SizedBox(
+                            width: _width * .29,
+                            child: const Text("01957073942"),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: _width * .30,
+                            child: Text(
+                              "Gender",
+                              style: Style.extrasmallerD14,
+                            ),
+                          ),
+                          Text(
+                            ": ",
+                            style: Style.extrasmallerD14,
+                          ),
+                          SizedBox(
+                            width: _width * .29,
+                            child: const Text(
+                              "male",
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 5,
                       ),
                     ],
                   ),
-                ),
-              ],
-            ),
-          ],
+                  SizedBox(
+                    width: _width * .15,
+                    child: Column(
+                      children: [
+                        Text(
+                          "A+",
+                          style: Style.average,
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        const Icon(
+                          Icons.call,
+                          size: 30,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
