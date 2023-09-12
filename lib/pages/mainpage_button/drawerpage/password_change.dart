a398c47670b3d79a7b0557fbe93d4240f2229fd9
Nurujjaman329 <mdnurujjaman329@gmail.com>
2023-09-12 13:37:46 +0600

    modify

--- lib/pages/mainpage_button/drawerpage/password_change.dart
+++ lib/pages/mainpage_button/drawerpage/password_change.dart
@@ -9,115 +9,117 @@ class PasswordChange extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 8.0),
-      child: Container(
-        alignment: Alignment.center,
-        height: MediaQuery.of(context).size.height / 2.4,
-        decoration: BoxDecoration(
-            color: Colors.white, borderRadius: BorderRadius.circular(15.0)),
-        child: Column(children: [
-          const Text(
-            "Change Password",
-            style: TextStyles.inside,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              textInputAction: TextInputAction.done,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(14.0),
+      child: SingleChildScrollView(
+        child: Container(
+          alignment: Alignment.center,
+          height: MediaQuery.of(context).size.height / 2.42,
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(15.0)),
+          child: Column(children: [
+            const Text(
+              "Change Password",
+              style: TextStyles.inside,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                textInputAction: TextInputAction.done,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(14.0),
+                    ),
                   ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: ColorCode.primary_color,
+                    ),
+                  ),
+                  hintText: "Old Password ",
+                  prefixIcon: Icon(
+                    Icons.security_rounded,
                     color: ColorCode.primary_color,
                   ),
                 ),
-                hintText: "Old Password ",
-                prefixIcon: Icon(
-                  Icons.security_rounded,
-                  color: ColorCode.primary_color,
-                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              textInputAction: TextInputAction.done,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(14.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                textInputAction: TextInputAction.done,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(14.0),
+                    ),
                   ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: ColorCode.primary_color,
+                    ),
+                  ),
+                  hintText: "New Password ",
+                  prefixIcon: Icon(
+                    Icons.security_rounded,
                     color: ColorCode.primary_color,
                   ),
                 ),
-                hintText: "New Password ",
-                prefixIcon: Icon(
-                  Icons.security_rounded,
-                  color: ColorCode.primary_color,
-                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              textInputAction: TextInputAction.done,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(14.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                textInputAction: TextInputAction.done,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(14.0),
+                    ),
                   ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: ColorCode.primary_color,
+                    ),
+                  ),
+                  hintText: "Re_Type Password ",
+                  prefixIcon: Icon(
+                    Icons.security_rounded,
                     color: ColorCode.primary_color,
                   ),
                 ),
-                hintText: "Re_Type Password ",
-                prefixIcon: Icon(
-                  Icons.security_rounded,
-                  color: ColorCode.primary_color,
-                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text(
-                    "Cancel",
-                    style: TextStyles.app_bar,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text(
+                      "Cancel",
+                      style: TextStyles.app_bar,
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  width: 15,
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text(
-                    "save",
-                    style: TextStyles.app_bar,
+                  const SizedBox(
+                    width: 15,
                   ),
-                ),
-              ],
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text(
+                      "save",
+                      style: TextStyles.app_bar,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ]),
+          ]),
+        ),
       ),
     );
   }
